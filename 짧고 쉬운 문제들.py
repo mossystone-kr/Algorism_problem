4b50311336218fca00594d3679e1e69535576809
minjunasdf <62454927+minjunasdf@users.noreply.github.com>
2022-12-31 22:33:40 +0900

    1157 및 쉬운 문제 풀이

--- 짧고 쉬운 문제들.py
+++ 짧고 쉬운 문제들.py
@@ -5,3 +5,22 @@ print(str(datetime.datetime.now())[:10])
 #10699 날짜 문제
 import datetime
 print(str(datetime.datetime.now())[:10])
+
+#1330 대소비교 문제
+A, B = map(int,input().split())
+if A>B:
+	print(">")
+elif A<B:
+	print("<")
+else:
+	print("==")
+
+#2438,2439 별 문제
+N = int(input())
+for i in range(N):
+    i += 1
+    K = N-i
+    print(" " * K + "*" * i)
+
+#2475 검증수 문제
+print(sum([n**2 for n in map(int, input().split())]) % 10)
