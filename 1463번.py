4fe3f515138db077e40abfcf99069f74363b8c6e
minjunasdf <62454927+minjunasdf@users.noreply.github.com>
2023-01-04 00:56:16 +0900

    문제 풀이

--- 1463번.py
+++ 1463번.py
@@ -1,11 +1,18 @@
 import sys
 
 a = int(sys.stdin.readline())
-data = [0,]
+data = [0,0,0,0,0,0,0,0,0,0]
 
-for i in range(a):
+for l in range(a+1):
+    data.append(0)
+
+
+for i in range(a+1):
     if data[i] != 0:
         break
-    data[i * 3]
+    data[i * 3] += 1
+    data[i * 2] += 1
+    data[i + 1] += 1
+    print(data[i])
 
-print(data[a])
+print(data[a+1])
