d29dd9238b72d13598b712eea5cab0c616834d2f
minjun <minjun060606@naver.com>
2023-01-25 19:05:18 +0900

    문제 풀이

--- 1260번 DFS와 BFS.py
+++ 1260번 DFS와 BFS.py
@@ -1,9 +1,29 @@
 import sys
 
 dot, line, start =  map(int,sys.stdin.readline().split())
-v1,v2 = [],[]
+graph = []
+data = []
+end = False
+
+def bfs(graph_input, start_node):
+    need_visited, visited = [],[]
+    need_visited.append(start_node)
+
+    while need_visited:
+        node = need_visited[0]
+        del need_visited[0]
+
+        for k in graph_input[node]:
+            if not k in visited:
+                visited.append(k)
+
+for _ in range(dot+1):
+    graph.append([])
 
 for _ in range(line):
     a, b = map(int,sys.stdin.readline().split())
-    v1.append(b)
-    v2.append(a)
+    graph[a].append(b)
+    graph[b].append(a)
+
+for i in graph:
+    i.sort()
