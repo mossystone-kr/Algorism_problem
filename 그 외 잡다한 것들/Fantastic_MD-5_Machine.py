a6227442f1de8930c352fb41c2900d4b7623c6a0
minjunasdf <62454927+minjunasdf@users.noreply.github.com>
2023-04-12 20:52:31 +0900

    판타스틱 MD-5 기계

--- 그 외 잡다한 것들/Fantastic_MD-5_Machine.py
+++ 그 외 잡다한 것들/Fantastic_MD-5_Machine.py
@@ -3,24 +3,23 @@ import time
 import os
 import random as rd
 
-
 magics = [
-    "화염 발사",            #
+    "화염 발사",  # 지속딜
     "마나 회복",
     "체력 회복",
-    "회복",
+    "회복 마법",
     "방어 마법",
     "반사 마법",
     "번개 마법",
-    "독 마법",              # 지속딜
-    "상대 체력 회복 마법",
-    "상대 마나 회복 마법",
-    "상대 회복 마법",
-    "저주 마법",            # 지속딜 + 힘 감소
-    "혼란 마법",            # 크리, 중첩 확률 감소
-    "불운 방법",            # 운 감소
-    "흡혈",                # 상대 체력 강탈
-    "마나 흡수"             # 상대 마나 강탈
+    "맹독 마법",  # 지속딜
+    "씨발 부활",
+    "각성 마법",
+    "씨발 자폭",  # 현제 자신 체력 - 1의 데미지를 모두에게 부여
+    "저주 마법",  # 지속딜 + 힘 감소
+    "혼란 마법",  # 크리, 중첩 확률 감소
+    "불운 방법",  # 운 감소
+    "흡혈 마법",  # 상대 체력 강탈
+    "마나 흡수"  # 상대 마나 강탈
 ]
 
 
@@ -28,28 +27,47 @@ class player:
     def __init__(self):
         self.name = foolish_fun
         self.md5 = foolish_fun
-        self.health = 0       # 체력
-        self.power = 0        # 힘           : 공격 확률
-        self.defence = 0      # 방어          : 대미지 반감 확률
-        self.speed = 0        # 회피          : 공격 무효화 확률
-        self.luck = 0         # 운            : 뭔가 정말 우연이 일어날 확률
-        self.mana = 0         # 마나          :
-        self.magic = 0        # 마법 강도      : 마법의 세기
-        self.magics = []      # 마법 종류
-        self.critical = 0     # 크리티컬 확률
-        self.critic = 0      # 크리티컬 배수
-        self.hit_rate = 0     # 중첩 공격 확률
+        self.health = 0  # 체력
+        self.power = 0  # 힘           : 공격 확률
+        self.defence = 0  # 방어          : 데미지 반감 확률
+        self.speed = 0  # 회피          : 공격 무효화 확률
+        self.luck = 0  # 정확도
+        self.mana = 0  # 마나          :
+        self.magic = 0  # 마법 강도      : 마법의 세기
+        self.magics = []  # 마법 종류
+        self.critical = 0  # 크리티컬 확률
+        self.critic = 0  # 크리티컬 배수
+        self.hit_rate = 0  # 중첩 공격 확률
+
+        self.original_health = 0
+        self.original_power = 0
+        self.original_defence = 0
+        self.original_speed = 0
+        self.original_luck = 0
+        self.original_mana = 0
+        self.original_magic = 0
+        self.original_critical = 0
+        self.original_critic = 0
+        self.original_hit_rate = 0
 
     def attack(self, ally):
         ply_persent = rd.randrange(1, 1000)
-        if self.power >= ply_persent:
+        if self.luck >= ply_persent:
             ply_persent2 = rd.randrange(1, 1000)
             if self.critical >= ply_persent2:
-                ally.health = ally.health - self.power * self.critic / 1000
-                return self.name, "크리티컬!!", self.power * self.critic / 10000, ally.name, ally.health / 10
+                ply_persent3 = rd.randrange(1, 1000)
+                if ally.speed >= ply_persent3:
+                    return self.name, "상대의 회피 성공!", 0, ally.name, ally.health / 10
+                else:
+                    ally.health = ally.health - self.power * self.critic / 1000
+                    return self.name, "크리티컬!!", self.power * self.critic / 10000, ally.name, ally.health / 10
             else:
-                ally.health = ally.health - self.power
-                return self.name, "일반 공격", self.power / 10, ally.name, ally.health / 10
+                ply_persent3 = rd.randrange(1, 1000)
+                if ally.speed >= ply_persent3:
+                    return self.name, "상대의 회피 성공!", 0, ally.name, ally.health / 10
+                else:
+                    ally.health = ally.health - self.power
+                    return self.name, "일반 공격", self.power / 10, ally.name, ally.health / 10
         else:
             return self.name, "공격 실패!", 0, ally.name, ally.health / 10
 
@@ -64,10 +82,10 @@ def typing(strigab, dsv=True):
 
 def foolish_fun(dsav, da=True):
     dsav = dsav / 10
-    if dsav/100 >= 1:
+    if dsav / 100 >= 1:
         return str(dsav)
     else:
-        if dsav/10 >= 1:
+        if dsav / 10 >= 1:
             if da:
                 return str(' ' + str(dsav))
             else:
@@ -84,14 +102,24 @@ def mapping_md5(adbad):
     adbad.health = int((int(adbad.md5[0] + adbad.md5[1], 16) + 1) * 4000 / 256 + 1000)
     adbad.defence = int((int(adbad.md5[2] + adbad.md5[3], 16) + 1) * 1000 / 256)
     adbad.power = int((int(adbad.md5[4] + adbad.md5[5], 16) + 1) * 1000 / 256)
-    adbad.speed = int((int(adbad.md5[6] + adbad.md5[7], 16) + 1) * 1000 / 256)
-    adbad.luck = int((int(adbad.md5[8] + adbad.md5[9], 16) + 1) * 1000 / 256)
+    adbad.speed = int((int(adbad.md5[6] + adbad.md5[7], 16) + 1) * 700 / 256)
+    adbad.luck = int((int(adbad.md5[8] + adbad.md5[9], 16) + 1) * 700 / 256) + 300
     adbad.mana = int((int(adbad.md5[10] + adbad.md5[11], 16) + 1) * 1000 / 256)
-    adbad.critical = int((int(adbad.md5[12] + adbad.md5[13], 16) + 1) * 1000 / 256)
-    adbad.critic = int((int(adbad.md5[14] + adbad.md5[15], 16) + 1) * 1000 / 256) + 1000
+    adbad.critical = int((int(adbad.md5[12] + adbad.md5[13], 16) + 1) * 300 / 256)
+    adbad.critic = int((int(adbad.md5[14] + adbad.md5[15], 16) + 1) * 8000 / 256) + 2000
     adbad.hit_rate = int((int(adbad.md5[16] + adbad.md5[17], 16) + 1) * 1000 / 256)
     adbad.magic = int((int(adbad.md5[18] + adbad.md5[19], 16) + 1) * 1000 / 256)
     adbad.magics = [int(adbad.md5[20], 16), int(adbad.md5[21], 16), int(adbad.md5[22], 16)]
+    adbad.original_health = adbad.health
+    adbad.original_power = 0
+    adbad.original_defence = 0
+    adbad.original_speed = 0
+    adbad.original_luck = 0
+    adbad.original_mana = 0
+    adbad.original_magic = 0
+    adbad.original_critical = 0
+    adbad.original_critic = 0
+    adbad.original_hit_rate = 0
 
 
 player1 = player()
@@ -111,27 +139,17 @@ typing("서로의 스텟")
 print("")
 print(player1.name + ' vs ' + player2.name)
 print("")
-time.sleep(1)
 print(foolish_fun(player1.health), '|', foolish_fun(player2.health, False), ": 체력")
-time.sleep(1)
 print(foolish_fun(player1.power), '|', foolish_fun(player2.power, False), ": 힘")
-time.sleep(1)
 print(foolish_fun(player1.defence), '|', foolish_fun(player2.defence, False), ": 방어")
-time.sleep(1)
 print(foolish_fun(player1.speed), '|', foolish_fun(player2.speed, False), ": 회피")
-time.sleep(1)
 print(foolish_fun(player1.luck), '|', foolish_fun(player2.luck, False), ": 운")
-time.sleep(1)
 print(foolish_fun(player1.mana), '|', foolish_fun(player2.mana, False), ": 마나")
-time.sleep(1)
 print(foolish_fun(player1.magic), '|', foolish_fun(player2.magic, False), ": 마법 세기")
-time.sleep(1)
 print(foolish_fun(player1.critical), '|', foolish_fun(player2.critical, False), ": 크리티컬 확률")
-time.sleep(1)
 print(foolish_fun(player1.critic), '|', foolish_fun(player2.critic, False), ": 크리티컬 배수")
-time.sleep(1)
 print(foolish_fun(player1.hit_rate), '|', foolish_fun(player2.hit_rate, False), ": 중첩 공격 확률")
-time.sleep(1.5)
+time.sleep(4)
 print("")
 print(player1.name, "의 마법 : ", end='')
 for i in player1.magics:
@@ -155,25 +173,40 @@ time.sleep(1)
 typing("게임을 시작합니다...")
 
 turn = 1
+
 while True:
     os.system("cls")
     print('%d 번째 턴' % turn)
     print('------------------------------------------')
-    print('%s의 공격\n %s %d 데미지\n %s의 남은 체력 : %d' % player1.attack(player2))
+    attack = player1.attack(player2)
+    print('%s의 공격\n %s %d 데미지\n %s의 남은 체력 : %d / %d' % (attack[0],
+                                                        attack[1],
+                                                        attack[2],
+                                                        attack[3],
+                                                        attack[4],
+                                                        player1.original_health / 10))
     print('------------------------------------------')
     if player2.health <= 0:
         os.system("cls")
         print(player1.name + "승리!")
         print('승자의 남은 체력 : %.2f ' % (player1.health / 10))
+        print('최후의 일격 : %s, %d 데미지' % (attack[1], attack[2]))
         print('진행한 턴 수 : %d' % turn)
         break
-    print('%s의 공격\n %s %d 데미지\n %s의 남은 체력 : %d' % player2.attack(player1))
+    attack = player2.attack(player1)
+    print('%s의 공격\n %s %d 데미지\n %s의 남은 체력 : %d / %d' % (attack[0],
+                                                        attack[1],
+                                                        attack[2],
+                                                        attack[3],
+                                                        attack[4],
+                                                        player2.original_health / 10))
     print('------------------------------------------')
     if player1.health <= 0:
         os.system("cls")
         print(player2.name + "승리!")
         print('승자의 남은 체력 : %.2f ' % (player2.health / 10))
+        print('최후의 일격 : %s, %d 데미지' % (attack[1], attack[2]))
         print('진행한 턴 수 : %d' % turn)
         break
-    time.sleep(2)
+    time.sleep(0.5)
     turn = turn + 1
